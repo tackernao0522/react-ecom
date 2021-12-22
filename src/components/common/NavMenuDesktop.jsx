102cb47ed659f49c52f0b8ce2468eb49cdc3d1f8
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-22 18:02:50 +0900

    289 Nofitication Page

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -70,7 +70,7 @@ class NavMenuDesktop extends Component {
                       <span className="badge text-white bg-danger">3</span>
                     </sup>
                   </Link>
-                  <Link to="/" className="btn">
+                  <Link to="/notification" className="btn">
                     <i className="fa h4 fa-bell"></i>
                     <sup>
                       <span className="badge text-white bg-danger">5</span>
