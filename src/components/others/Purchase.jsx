aa4d06887264126697ca4f8f6b1cc2e82b913e96
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-25 15:32:24 +0900

    308 Consume Site Info API in Client Side Part2

--- src/components/others/Purchase.jsx
+++ src/components/others/Purchase.jsx
@@ -1,7 +1,27 @@
+import axios from 'axios'
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import ReactHtmlParser from 'react-html-parser'
+import AppURL from '../../api/AppURL'
 
 class Purchase extends Component {
+  constructor() {
+    super()
+    this.state = {
+      parchase: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get(AppURL.AllSiteInfo).then((resp) => {
+      let StatusCode = resp.status
+      if (StatusCode == 200) {
+        let JsonData = resp.data[0]['parchase_guide']
+        this.setState({ parchase: JsonData })
+      }
+    })
+  }
+
   render() {
     return (
       <Fragment>
@@ -14,23 +34,7 @@ class Purchase extends Component {
               sm={12}
               xs={12}
             >
-              <h4 className="section-tile-login">Purchase Page</h4>
-              <p className="section-title-contact">
-                Hi! I'm Kazi Ariyan. I'm a web developer with a serious love for
-                teaching I am a founder of easy Learning and a passionate Web
-                Developer, Programmer & Instructor.<br /><br />
-                I am working online for the last 9 years and have created several successful websites
-                running on the internet.<br /><br />
-                I try to create a project-based course that helps you to learn professionally and make you fell as a
-                complete developer. easy learning exists to help you succeed in
-                life. Each course has been hand-tailored to teach a specific
-                skill.<br /><br />
-                I hope you agree! Whether you’re trying to learn a new
-                skill from scratch or want to refresh your memory on something
-                you’ve learned in the past, you’ve come to the right place.
-                Education makes the world a better place. Make your world better
-                with new skills.
-              </p>
+              {ReactHtmlParser(this.state.parchase)}
             </Col>
           </Row>
         </Container>
