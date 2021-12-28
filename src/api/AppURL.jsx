afdbf078ccfeaa7d7eee636dff51c5fae5db6afb
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-28 19:25:31 +0900

    329 Consume API For Home Slider

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -14,6 +14,7 @@ class AppURL {
       this.BaseURL + '/productlistbysubcategory/' + category + '/' + subcategory
     )
   }
+  static AllSlider = this.BaseURL + '/allslider'
 }
 
 export default AppURL
