c90877ef52236e7e535f2d9829e77b6ead67d361
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-29 17:34:14 +0900

    332 Managing Loader Placeholder Part2

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -3,12 +3,15 @@ import React, { Component, Fragment } from 'react'
 import { Card, Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import AppURL from '../../api/AppURL'
+import FeaturedLoading from '../placeholder/FeaturedLoading'
 
 class FeaturedProducts extends Component {
   constructor() {
     super()
     this.state = {
       ProductData: [],
+      isLoading: '',
+      mainDiv: 'd-none',
     }
   }
 
@@ -16,7 +19,11 @@ class FeaturedProducts extends Component {
     axios
       .get(AppURL.ProductListByRemark('FEATURED'))
       .then((resp) => {
-        this.setState({ ProductData: resp.data })
+        this.setState({
+          ProductData: resp.data,
+          isLoading: 'd-none',
+          mainDiv: '',
+        })
       })
       .catch((error) => {
         console.log(error)
@@ -62,14 +69,17 @@ class FeaturedProducts extends Component {
 
     return (
       <Fragment>
-        <Container className="text-center" fluid={true}>
-          <div className="section-title text-center mb-55">
-            <h2>FEATURED PRODUCT</h2>
-            <p>Some Of Our Exclusive Collection, You May Like</p>
-          </div>
+        <FeaturedLoading isLoading={this.state.isLoading} />
+        <div className={this.state.mainDiv}>
+          <Container className="text-center" fluid={true}>
+            <div className="section-title text-center mb-55">
+              <h2>FEATURED PRODUCT</h2>
+              <p>Some Of Our Exclusive Collection, You May Like</p>
+            </div>
 
-          <Row>{MyView}</Row>
-        </Container>
+            <Row>{MyView}</Row>
+          </Container>
+        </div>
       </Fragment>
     )
   }
