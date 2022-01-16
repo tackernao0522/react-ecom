a350a1f4020b8ed5149881f193f4410bf29e17d7
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-01-16 15:21:16 +0900

    397 Remove Item From Favorite Part2

--- src/components/favorite/Favorite.jsx
+++ src/components/favorite/Favorite.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import cogoToast from 'cogo-toast'
 import React, { Component, Fragment } from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import { Redirect } from 'react-router-dom'
 import AppURL from '../../api/AppURL'
 
 class Favorite extends Component {
@@ -10,10 +12,13 @@ class Favorite extends Component {
       ProductData: [],
       isLoading: '',
       mainDiv: 'd-none',
+      PageRefreshStatus: false,
     }
   }
 
   componentDidMount() {
+    window.scroll(0, 0)
+
     axios
       .get(AppURL.FavoriteList(this.props.user.email))
       .then((res) => {
@@ -28,15 +33,43 @@ class Favorite extends Component {
       })
   }
 
+  removeItem = (event) => {
+    let product_code = event.target.getAttribute('data-code')
+    let email = this.props.user.email
+
+    axios
+      .get(AppURL.FavoriteRemove(product_code, email))
+      .then((res) => {
+        cogoToast.success('Product Item Remove', { position: 'top-right' })
+        this.setState({ PageRefreshStatus: true })
+      })
+      .catch((error) => {
+        cogoToast.error('Your Request is not done ! Try Again', {
+          postion: 'top-right',
+        })
+      })
+  }
+
+  PageRefresh = () => {
+    if (this.state.PageRefreshStatus === true) {
+      let URL = window.location
+      return <Redirect to={URL} />
+    }
+  }
+
   render() {
     const FavList = this.state.ProductData
     const MyView = FavList.map((ProductList, i) => (
-      <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+      <Col key={i.toString()} className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
         <Card className="image-box card w-100">
           <img className="center w-75" src={ProductList.image} />
           <Card.Body>
             <p className="product-name-on-card">{ProductList.product_name}</p>
-            <Button className="btn btn-sm">
+            <Button
+              onClick={this.removeItem}
+              data-code={ProductList.product_code}
+              className="btn btn-sm"
+            >
               <i className="fa fa-trash-alt"></i> Remove
             </Button>
           </Card.Body>
@@ -54,6 +87,7 @@ class Favorite extends Component {
 
           <Row>{MyView}</Row>
         </Container>
+        {this.PageRefresh()}
       </Fragment>
     )
   }
